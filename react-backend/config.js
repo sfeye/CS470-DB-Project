093885c3718e4603e72005f30ebc6d6aaa654069
Sam Feye <samfeye@gmail.com>
2020-04-05 11:03:38 -0500

    Fix all vulnerabilities and added linting reqs
    replaced jade with pug, updated react-script, installed eslint, created config file for eslint, ect...

--- react-backend/config.js
+++ react-backend/config.js
@@ -2,7 +2,7 @@ var config = {
     host: "localhost",
     user: "root",
     // change this to your password
-    password: "",
+    password: "beamer08271996",
     database: "library"
   };
   module.exports = config;
